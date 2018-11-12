cb252958f324f1834607e7478ed69d768bbcad93
Tatiana <tatiana.c.pena1@gmail.com>
2018-11-12 00:09:13 -0600

    created chatbox model

--- app/models/chatbox.rb
+++ app/models/chatbox.rb
@@ -1,2 +1,10 @@
 class Chatbox < ApplicationRecord
+  belongs_to :conversation
+  belongs_to :user
+
+  validates_presence_of :body, :conversation_id, :user_id
+
+  def friendly_time
+    created_at.strftime("%e %b %Y %H:%M:%S%p")
+  end
 end
